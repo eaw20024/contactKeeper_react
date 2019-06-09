5a66bb284c2f86ee33b1d98c5fce9079f49c1a76
Eric Washington <eric.washington.me@gmail.com>
2019-06-09 17:32:19 -0400

    Contact Model & Get Contacts Route

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const config = require('config')
 const auth = require('../middleware/auth')
 const { check, validationResult } = require('express-validator/check');
-const config = require('config')
 
 const User = require('../models/User')
 
