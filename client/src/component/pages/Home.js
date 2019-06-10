17e7e11dbcbf6c7d35ed6aa94943f84a64c27c9f
Eric Washington <eric.washington.me@gmail.com>
2019-06-10 13:35:37 -0400

    Contact Filter Form & State

--- client/src/component/pages/Home.js
+++ client/src/component/pages/Home.js
@@ -1,6 +1,7 @@
 import React from '../../../node_modules/react'
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm'
+import ContactFilter from '../contacts/ContactFilter'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
                 <ContactForm />
             </div>
         <div>
+            <ContactFilter />
             <Contacts />
         </div>
         </div>
