73517d9b7c014a9bbc02684f8c78a1a3083155b3
Eric Washington <eric.washington.me@gmail.com>
2019-06-10 10:27:47 -0400

    Setup React, Add Concurrently, Navbar & Router Comp Setup

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter  as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './component/layout/Navbar';
+import './App.css';
+import Home from './component/pages/Home'
+import About from './component/pages/About'
+
+const App = () => {
+  return (
+    <Router>
+    <Fragment>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </div>
+    </Fragment>
+    </Router>
+  );
+}
+
+export default App;
